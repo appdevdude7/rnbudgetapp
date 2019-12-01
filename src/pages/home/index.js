a9a1410ee7fa7834a562b22411648096b979024f
appdevdude7 <appdeveloper.akshay@gmail.com>
2019-12-01 16:02:59 -0600

    updated app along with patches

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,12 +1,37 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { PureComponent } from 'react';
+import { StatusBar, SafeAreaView, View, FlatList } from 'react-native';
+import { page } from '../../utils/styles';
+import EventCategory from '../../components/event-category';
+import Hero from '../../components/home-hero';
+import Grid from '../../components/event-category/grid';
+import { events } from '../../models';
 
-export default class Home extends Component {
+
+export default class home extends PureComponent {
+
     render() {
+        const lastIndex = events.length - 1;
         return (
-            <View>
-                <Text> Hello </Text>
-            </View>
+            <>
+                <StatusBar barStyle="light-content" backgroundColor="#685BA900" translucent={true} />
+
+                <SafeAreaView style={page} >
+                    <FlatList
+                        data={events}
+                        initialNumToRender={2}
+                        showsVerticalScrollIndicator={false}
+                        ListHeaderComponent={() => <Hero />}
+                        ItemSeparatorComponent={() => <View style={{ height: 1, backgroundColor: '#dadada', marginBottom: 30 }} />}
+                        ListItemComponent={({ index, item }) =>
+                            index != lastIndex ?
+                                <EventCategory events={item.events} title={item.title} /> :
+                                <Grid events={item.events} title={item.title} />
+                        }
+                        keyExtractor={(_, index) => index.toString()}
+                    />
+
+                </SafeAreaView>
+            </>
         )
     }
 }
---
+++ src/utils/styles.js
@@ -0,0 +1,6 @@
+const page = {
+    flex:1,
+    backgroundColor:'#fff'
+};
+
+export {page}
